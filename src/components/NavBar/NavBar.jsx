7cb0bf70b779b07955e0259d5ac7488c30152aba
Nicolas Messina <nicolasmessina25@gmail.com>
2022-09-07 13:52:35 -0300

    Implementacion del componente NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import './NavBar.scss';
+import '../../scss/style.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHouse, faCartShopping, faShirt, faMugSaucer, faChild, faCirclePlus} from '@fortawesome/free-solid-svg-icons';
+
+//import logo from '../../../public/logo192.png'
+
+function NavBar(){
+    return (
+        <nav className='flexible--row'>
+            <img src="img/swShopLogo.png" alt="" />
+            <button className='button2--blue'><FontAwesomeIcon icon={faHouse}/> Inicio</button>
+            <ul className="menu flexible--row">
+                <li className='button--blue'><FontAwesomeIcon icon={faChild}/> Figuras de Acción</li>
+                <li className='button--blue'><FontAwesomeIcon icon={faShirt}/> Ropa</li>
+                <li className='button--blue'><FontAwesomeIcon icon={faMugSaucer}/> Tazas</li>
+                <li className='button--blue'><FontAwesomeIcon icon={faCirclePlus}/> Otros</li>
+            </ul>
+            <button className='button3--green cart'><FontAwesomeIcon icon={faCartShopping}/></button>
+        </nav>
+    );
+}
+
+export default NavBar;
